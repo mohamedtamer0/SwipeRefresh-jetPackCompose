db4c0d1f3b19802ca5ec5440cf71fa528e7f891a
mohamedtamer0 <mt4452242@gmail.com>
2022-01-25 02:11:46 +0200

    update

--- app/src/main/java/com/example/swiperefresh_jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/swiperefresh_jetpackcompose/MainActivity.kt
@@ -3,12 +3,29 @@ package com.example.swiperefresh_jetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.swiperefresh_jetpackcompose.ui.theme.SwipeRefreshjetPackComposeTheme
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +34,7 @@ class MainActivity : ComponentActivity() {
             SwipeRefreshjetPackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    SwipeRefreshCompose()
                 }
             }
         }
@@ -25,14 +42,88 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun SwipeRefreshCompose() {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SwipeRefreshjetPackComposeTheme {
-        Greeting("Android")
+    Scaffold(
+        topBar = {
+
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "SwipeRefresh",
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        color = Color.White
+                    )
+                },
+                backgroundColor = Color.Black
+            )
+        }, modifier = Modifier.fillMaxSize()
+
+    ) {
+
+        var refreshing by remember { mutableStateOf(false) }
+        LaunchedEffect(refreshing) {
+            if (refreshing) {
+                delay(2000)
+                refreshing = false
+            }
+        }
+
+
+        SwipeRefresh(
+            state = rememberSwipeRefreshState(isRefreshing = refreshing),
+            onRefresh = { refreshing = true }) {
+            LazyColumn {
+                items(count = 10) {
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(100.dp)
+                            .padding(10.dp, 5.dp, 10.dp, 5.dp)
+                            .clip(
+                                RoundedCornerShape(10.dp)
+                            )
+                            .background(Color.LightGray)
+                    ) {
+                        Column(modifier = Modifier.padding(10.dp)) {
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                Image(
+                                    painter = painterResource(R.drawable.dog),
+                                    contentDescription = "Image",
+                                    contentScale = ContentScale.Crop,
+                                    modifier = Modifier
+                                        .size(60.dp)
+                                        .clip(CircleShape)
+                                )
+
+                                Spacer(modifier = Modifier.padding(5.dp))
+
+                                Column {
+                                    Text(
+                                        text = "Sample Test",
+                                        color = Color.White,
+                                        fontSize = 16.sp,
+                                        fontWeight = FontWeight.Bold
+                                    )
+
+                                    Spacer(modifier = Modifier.padding(2.dp))
+
+                                    Text(
+                                        text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                                        color = Color.Gray,
+                                        fontSize = 12.sp
+                                    )
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
     }
+
 }
